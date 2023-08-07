840a7accb06e9db1adf9244039d4f5e76577f76b
kartik-dhawan <kartikkdhawan@gmail.com>
2023-08-08 01:54:44 +0530

    Integrated models & added first table

--- backend/api/views/books.py
+++ backend/api/views/books.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from books.models import Book
+from django.forms.models import model_to_dict
 import json
 
 # Create your views here.
@@ -7,11 +9,23 @@ import json
 def get_all_books(request, *args, **kwargs):
     print(request.GET)
 
+    all_books = Book.objects.all()
+    first_book = Book.objects.all().first()
+    len = int(model_to_dict(Book.objects.all().last())['id'])
+    print('len')
+    print(len)
+
     data = {}
     # data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-    data['method'] = request.method
-    data['results'] = [{"name": "kartik"}]
-    data['params'] = dict(request.GET)
-    data['body'] = dict(json.loads(request.body))
-    return JsonResponse(data)
+    # data['content_type'] = request.content_type
+    # data['method'] = request.method
+    # data['results'] = [{"name": "kartik"}]
+    # data['params'] = dict(request.GET)
+    # data['body'] = dict(json.loads(request.body))
+
+    if all_books:
+        global res
+        # res = model_to_dict()
+        res = all_books
+
+    return JsonResponse(model_to_dict(first_book))
